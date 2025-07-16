c9c5d2c9fa9550dd3e1abc48d9d78f6a0824c715
Kunal Singla <singlakunal55@gmail.com>
2025-07-16 19:03:44 +0530

    base url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from 'dayjs'
 import { toast } from 'react-toastify'
 
 export const api = axios.create({
-  baseURL: "https://propvest-kj13s3tbf-singlakunal55-9562s-projects.vercel.app/api",
+  baseURL: "https://propvest.vercel.app//api",
 })
 
 export const getAllProperties = async () => {
